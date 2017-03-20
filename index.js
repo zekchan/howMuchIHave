d8fd3ce377e6275de023a9904bd6eee53269119e
Ilya Groshev <groshev.i.d@gmail.com>
2017-03-20 22:41:35 +0300

    v0.0.1

---
+++ index.js
@@ -0,0 +1,21 @@
+const fetch = require('node-fetch');
+
+const I_HAVE = require('./balance.json');
+
+Promise.all(
+    Object.keys(I_HAVE).map(ticker => fetch(`https://api.coinmarketcap.com/v1/ticker/${ticker}/?convert=btc`)
+        .then(res => res.json())
+        .then(([{ price_usd, price_btc }]) => {
+            console.log(price_btc, price_usd);
+            return ({
+                btc: price_btc * I_HAVE[ticker],
+                usd: price_usd * I_HAVE[ticker]
+            })
+        })
+    )
+)
+    .then(values => console.log(values.reduce(({ totalUsd, totalBtc }, { btc, usd }) => ({
+            totalUsd: totalUsd + usd,
+            totalBtc: totalBtc + btc
+        }), { totalBtc: 0, totalUsd: 0 })
+    ));
